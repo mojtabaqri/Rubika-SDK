97be1be60abacb78186facf3bba4db1635f3cbe1
Mojtaba Gharibreza <mojtabaqri@gmail.com>
2026-07-04 15:20:08 +0330

    U

--- src/RequestLogger.php
+++ src/RequestLogger.php
@@ -4,8 +4,13 @@ namespace RubikaBot;
 
 function saveIncomingRequest(array $data): void
 {
-    $logFile = __DIR__ . '/../req.txt';
+    $logFile = dirname(__DIR__) . '/req.txt';
     $timestamp = date('Y-m-d H:i:s');
     $entry = "[{$timestamp}]" . PHP_EOL . var_export($data, true) . PHP_EOL;
-    file_put_contents($logFile, $entry, LOCK_EX);
+
+    $written = @file_put_contents($logFile, $entry, FILE_APPEND | LOCK_EX);
+    if ($written === false) {
+        $tempFile = sys_get_temp_dir() . '/rubika_req.txt';
+        @file_put_contents($tempFile, $entry, FILE_APPEND | LOCK_EX);
+    }
 }
