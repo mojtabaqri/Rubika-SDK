70d07ffcf55163881cc3a9f1b9fe29adc4b14d26
Mojtaba Gharibreza <mojtabaqri@gmail.com>
2026-07-04 19:44:37 +0330

    Revert "U"
    This reverts commit 97be1be60abacb78186facf3bba4db1635f3cbe1.

--- src/RequestLogger.php
+++ src/RequestLogger.php
@@ -4,13 +4,8 @@ namespace RubikaBot;
 
 function saveIncomingRequest(array $data): void
 {
-    $logFile = dirname(__DIR__) . '/req.txt';
+    $logFile = __DIR__ . '/../req.txt';
     $timestamp = date('Y-m-d H:i:s');
     $entry = "[{$timestamp}]" . PHP_EOL . var_export($data, true) . PHP_EOL;
-
-    $written = @file_put_contents($logFile, $entry, FILE_APPEND | LOCK_EX);
-    if ($written === false) {
-        $tempFile = sys_get_temp_dir() . '/rubika_req.txt';
-        @file_put_contents($tempFile, $entry, FILE_APPEND | LOCK_EX);
-    }
+    file_put_contents($logFile, $entry, LOCK_EX);
 }
